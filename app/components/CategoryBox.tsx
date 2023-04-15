d7c6e9d4545ab3d9cc9e44415d3f04694e2ef7b5
JA54312 <ija54312@gmail.com>
2023-04-15 10:30:52 -0600

    categories

---
+++ app/components/CategoryBox.tsx
@@ -0,0 +1,70 @@
+"use client";
+//Librerias
+import React, { useCallback } from "react";
+//Hooks
+import { useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
+//Models
+import { IconType } from "react-icons/lib";
+interface CategoryBoxProps {
+  icon: IconType;
+  label: string;
+  selected: boolean;
+}
+
+const CategoryBox: React.FC<CategoryBoxProps> = ({
+  icon: Icon,
+  label,
+  selected,
+}) => {
+  const router = useRouter();
+  const params = useSearchParams();
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+
+    const updateQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      delete updateQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updateQuery,
+      },
+      { skipNull: true }
+    );
+    router.push(url);
+  }, [label, params, router]);
+  return (
+    <div
+      onClick={handleClick}
+      className={`
+    flex 
+    flex-col 
+    items-center 
+    justify-center 
+    gap-2
+    p-3
+    border-b-2
+    hover:text-neutral-800
+    transition
+    cursor-pointer
+    ${selected ? "border-b-neutral-800" : "border-transparent"}
+    ${selected ? "text-neutral-800" : "text-neutral-500"}
+  `}
+    >
+      <Icon size={26} />
+      <div className="font-medium text-sm">{label}</div>
+    </div>
+  );
+};
+
+export default CategoryBox;
